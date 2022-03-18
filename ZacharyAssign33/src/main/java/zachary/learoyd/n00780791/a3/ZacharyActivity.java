2a45645dd7984eb879f33ec8d88a6824abd0ad12
ZacharyLearoyd0791 <zachary.learoyd@gmail.com>
2022-03-18 16:04:52 -0400

    Initial commit

---
+++ ZacharyAssign33/src/main/java/zachary/learoyd/n00780791/a3/ZacharyActivity.java
@@ -0,0 +1,15 @@
+//Zachary Learoyd N00780791 CENG258-RNB
+package zachary.learoyd.n00780791.a3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ZacharyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
